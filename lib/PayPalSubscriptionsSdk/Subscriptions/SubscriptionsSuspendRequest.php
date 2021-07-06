f2129efe7e86c5a32eda0c643650da4a148854e4
samuel petersen <samuelkpeter@gmail.com>
2021-07-06 11:45:26 -0300

    Fix Suspend Subscription Request

--- lib/PayPalSubscriptionsSdk/Subscriptions/SubscriptionsSuspendRequest.php
+++ lib/PayPalSubscriptionsSdk/Subscriptions/SubscriptionsSuspendRequest.php
@@ -10,7 +10,7 @@ class SubscriptionsSuspendRequest extends HttpRequest
 {
     function __construct($subscriptionId)
     {
-        parent::__construct("/v1/billing/subscriptions/{subscription_id}/cancel", "POST");
+        parent::__construct("/v1/billing/subscriptions/{subscription_id}/suspend", "POST");
         $this->path = str_replace("{subscription_id}", urlencode($subscriptionId), $this->path);
         $this->headers["Content-Type"] = "application/json";
     }
